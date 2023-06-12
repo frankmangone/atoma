7ad2e489857bc7e01036137a764a24527e2c754a
agrofrank <franco.mangone@agrotoken.io>
2023-06-12 19:20:48 -0300

    Adds `CompoundProperty` schema

---
+++ atoma-api/src/schemas/compound-property.schema.ts
@@ -0,0 +1,27 @@
+import { plainToInstance } from 'class-transformer';
+import { Paginated } from '@common/pagination/paginated.schema';
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Schema as Sch } from 'mongoose';
+import { BaseEntity } from '@common/repositories/base.schema';
+import { Compound } from './compound.schema';
+import { Property } from './property.schema';
+
+@ObjectType()
+@Schema()
+export class CompoundProperty extends BaseEntity {
+  static from(object: Document<CompoundProperty>): CompoundProperty {
+    return plainToInstance(CompoundProperty, object.toObject());
+  }
+
+  @Prop({ type: Sch.Types.ObjectId, ref: Compound.name })
+  compound: Compound;
+
+  @Prop({ type: Sch.Types.ObjectId, ref: Property.name })
+  property: Property;
+}
+
+@ObjectType()
+export class PaginatedCompounds extends Paginated(CompoundProperty) {}
+
+export const CompoundSchema = SchemaFactory.createForClass(Compound);
