068d7cf915a52ea2b5ad6c44416dfff04f5c8ead
agrofrank <franco.mangone@agrotoken.io>
2023-06-20 21:26:15 -0300

    Changes the Paginated type schema definition

--- atoma-api/src/common/graphql/pagination/paginated.schema.ts
+++ atoma-api/src/common/graphql/pagination/paginated.schema.ts
@@ -1,23 +1,41 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType, Int } from '@nestjs/graphql';
 import { Type } from '@nestjs/common';
 
+interface EdgeType<T> {
+  cursor: string;
+  node: T;
+}
+
 export interface PaginatedType<T> {
-  data: T[];
-  nextCursor: string | null;
-  prevCursor: string | null;
+  edges: EdgeType<T>[];
+  nodes: T[];
+  totalCount: number;
+  hasNextPage: boolean;
 }
 
 export function Paginated<T>(classRef: Type<T>): Type<PaginatedType<T>> {
-  @ObjectType({ isAbstract: true })
-  abstract class PaginatedModel implements PaginatedType<T> {
-    @Field(() => [classRef], { nullable: true })
-    data: T[];
+  @ObjectType(`${classRef.name}Edge`)
+  abstract class EdgeType {
+    @Field(() => String)
+    cursor: string;
 
-    @Field(() => String, { nullable: true })
-    nextCursor: string | null;
-
-    @Field(() => String, { nullable: true })
-    prevCursor: string | null;
+    @Field(() => classRef)
+    node: T;
   }
-  return PaginatedModel as Type<PaginatedType<T>>;
+
+  @ObjectType({ isAbstract: true })
+  abstract class PaginatedSchema implements PaginatedType<T> {
+    @Field(() => [EdgeType], { nullable: true })
+    edges: EdgeType[];
+
+    @Field(() => [classRef], { nullable: true })
+    nodes: T[];
+
+    @Field(() => Int)
+    totalCount: number;
+
+    @Field()
+    hasNextPage: boolean;
+  }
+  return PaginatedSchema as Type<PaginatedType<T>>;
 }
--- atoma-api/src/common/graphql/pagination/pagination.input.ts
+++ atoma-api/src/common/graphql/pagination/pagination.input.ts
@@ -3,10 +3,7 @@ import { InputType, Field, Int } from '@nestjs/graphql';
 @InputType()
 export class FindPaginatedInput {
   @Field(() => Int)
-  limit: number;
-
-  @Field(() => String, { nullable: true })
-  before?: string;
+  first: number;
 
   @Field(() => String, { nullable: true })
   after?: string;
