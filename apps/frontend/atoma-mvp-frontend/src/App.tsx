e00c256bb718b91b7fec2e744d2e38da1ff4f27f
agrofrank <franco.mangone@agrotoken.io>
2023-06-22 18:00:15 -0300

    Throws in a couple basic styles

--- apps/frontend/atoma-mvp-frontend/src/App.tsx
+++ apps/frontend/atoma-mvp-frontend/src/App.tsx
@@ -1,27 +1,43 @@
-import type { Component } from 'solid-js';
+import type { Component } from "solid-js";
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import logo from "./logo.svg";
+import styles from "./App.module.css";
 
 const App: Component = () => {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div class={styles.App}>
+			<header class={styles.header}>
+				<img src={logo} class={styles.logo} alt="logo" />
+				<h1>ATOMA</h1>
+				<h3>Compound property search & estimation</h3>
+				<div class={styles.card}>
+					<label for="compound" class={styles["input-group"]}>
+						Compound
+						<input type="text" name="compound" placeholder="Compound..." />
+					</label>
+					<label for="property" class={styles["input-group"]}>
+						Property
+						<input type="text" name="property" placeholder="Property..." />
+					</label>
+				</div>
+				<div class={styles.card}>
+					<div>
+						<label for="compound" class={styles["input-group"]}>
+							Compound
+							<input type="text" name="compound" placeholder="Compound..." />
+						</label>
+						<label for="property" class={styles["input-group"]}>
+							Property
+							<input type="text" name="property" placeholder="Property..." />
+						</label>
+					</div>
+					<div>
+						<h3>Estimate!</h3>
+					</div>
+				</div>
+			</header>
+		</div>
+	);
 };
 
 export default App;
