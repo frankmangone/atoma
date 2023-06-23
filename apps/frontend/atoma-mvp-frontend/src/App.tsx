cc0d46686bb21c1db90e97bde3d2d424ac2384c5
agrofrank <franco.mangone@agrotoken.io>
2023-06-23 09:38:06 -0300

    Reorganizes inputs a bit

--- apps/frontend/atoma-mvp-frontend/src/App.tsx
+++ apps/frontend/atoma-mvp-frontend/src/App.tsx
@@ -6,36 +6,75 @@ import styles from "./App.module.css";
 const App: Component = () => {
 	return (
 		<div class={styles.App}>
-			<header class={styles.header}>
+			<main class={styles.container}>
 				<img src={logo} class={styles.logo} alt="logo" />
 				<h1>ATOMA</h1>
 				<h3>Compound property search & estimation</h3>
 				<div class={styles.card}>
-					<label for="compound" class={styles["input-group"]}>
-						Compound
-						<input type="text" name="compound" placeholder="Compound..." />
-					</label>
-					<label for="property" class={styles["input-group"]}>
-						Property
-						<input type="text" name="property" placeholder="Property..." />
-					</label>
+					<div class={styles["input-group"]} style={{ "flex-basis": "250px" }}>
+						<label for="compound" class={styles.label}>
+							Compound
+						</label>
+
+						<input
+							type="text"
+							name="compound"
+							class={styles.input}
+							placeholder="Compound..."
+						/>
+					</div>
+
+					<div class={styles["input-group"]} style={{ "flex-basis": "250px" }}>
+						<label for="property" class={styles.label}>
+							Property
+						</label>
+
+						<input
+							type="text"
+							name="property"
+							class={styles.input}
+							placeholder="Property..."
+						/>
+					</div>
 				</div>
 				<div class={styles.card}>
-					<div>
-						<label for="compound" class={styles["input-group"]}>
-							Compound
-							<input type="text" name="compound" placeholder="Compound..." />
-						</label>
-						<label for="property" class={styles["input-group"]}>
-							Property
-							<input type="text" name="property" placeholder="Property..." />
-						</label>
+					<div
+						style={{
+							"flex-basis": "50%",
+							gap: "20px",
+							display: "flex",
+							"flex-direction": "column",
+							"align-items": "flex-end",
+						}}
+					>
+						<div class={styles["input-group"]} style={{ width: "250px" }}>
+							<label for="temperature" class={styles.label}>
+								Temperature
+							</label>
+							<input
+								type="text"
+								class={styles.input}
+								name="temperature"
+								placeholder="Temperature..."
+							/>
+						</div>
+						<div class={styles["input-group"]} style={{ width: "250px" }}>
+							<label for="pressure" class={styles.label}>
+								Pressure
+							</label>
+							<input
+								type="text"
+								class={styles.input}
+								name="pressure"
+								placeholder="Pressure..."
+							/>
+						</div>
 					</div>
-					<div>
+					<div style={{ "flex-basis": "50%" }}>
 						<h3>Estimate!</h3>
 					</div>
 				</div>
-			</header>
+			</main>
 		</div>
 	);
 };
