9200a3177a669c5100c8166eb0ad4b47da600933
agrofrank <franco.mangone@agrotoken.io>
2023-06-25 16:46:49 -0300

    Adds basic input component

---
+++ apps/frontend/atoma-components/components/input/styles.tsx
@@ -0,0 +1,21 @@
+import { styled } from "solid-styled-components";
+
+export const Wrapper = styled("div")`
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	font-size: 12px;
+	gap: 8px;
+`;
+
+export const Label = styled("label")`
+	font-weight: 700;
+	color: #5d6f67;
+`;
+
+export const Input = styled("input")`
+	border: 2px solid #5d6f67;
+	border-radius: 4px;
+	padding: 8px 12px;
+	align-self: stretch;
+`;
